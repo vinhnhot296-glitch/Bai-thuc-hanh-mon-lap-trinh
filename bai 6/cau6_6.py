76eac14b1226f0326ef63791a12dd2f6c7ae5502
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-11-13 19:22:20 +0700

    Add files via upload

---
+++ bai 6/cau6_6.py
@@ -0,0 +1,9 @@
+class IOString:
+    def __init__(self):
+        self.str1 = ""
+
+    def get_String(self, s):
+        self.str1 = s
+
+    def print_String(self):
+        print(self.str1.upper())

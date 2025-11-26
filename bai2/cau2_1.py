91813d5bbf61aecf3bed4c63c6aa4773ef7c8e09
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-11-26 20:58:50 +0700

    Add files via upload

---
+++ bai2/cau2_1.py
@@ -0,0 +1,8 @@
+print("sinh viên: trần huy vinh ")
+print("MSV:245752021610007")
+n1=int(input("enter n1 value"))
+n2=int(input("enter n2 value"))
+if n1>n2:
+    print("n1 í big")
+else:
+    print("n2 ís big")

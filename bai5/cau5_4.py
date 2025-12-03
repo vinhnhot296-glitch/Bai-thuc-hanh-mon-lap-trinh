64df1941e769f9df72ce84aeb27e5525bd6f01bc
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-12-03 07:45:07 +0700

    Update cau5_4.py

--- bai5/cau5_4.py
+++ bai5/cau5_4.py
@@ -1,3 +1,4 @@
+print('họ tên: trần huy vinh; MSSV:245752021610007')
 import numpy as np
 
 # Tạo mảng với các giá trị từ 12 đến 38 (không bao gồm 38)
@@ -9,4 +10,5 @@ print(arr)
 reversed_arr = arr[::-1]
 print("Mảng đảo ngược:")
 print(reversed_arr)
+
 

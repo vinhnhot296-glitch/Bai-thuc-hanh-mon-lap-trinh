b608fe96e0a55555c463f5d05b82af925f2d9767
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-12-03 07:37:58 +0700

    Include student info and reformat input prompts
    Added student information and formatted the input prompts.

--- bai4/cau4_13.py
+++ bai4/cau4_13.py
@@ -1,3 +1,4 @@
+print('họ tên: trần huy vinh; MSSV:245752021610007')
 ds = input("Nhập các phần tử của list (cách nhau bằng khoảng trắng): ").split()
 print("\nList ban đầu:", ds)
 tim = input("\nNhập phần tử muốn tìm kiếm: ")

d3c1967aad08e3f3b6db97810b31324eab64a2b6
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-12-03 07:38:10 +0700

    Update cau4_14.py

--- bai4/cau4_14.py
+++ bai4/cau4_14.py
@@ -1,4 +1,6 @@
+print('họ tên: trần huy vinh; MSSV:245752021610007')
 ds = input("Nhập các phần tử của list (cách nhau bằng khoảng trắng): ").split()
 print("\nList ban đầu:", ds)
 ds.sort()
 print("\nList sau khi sắp xếp:", ds)
+

d6d3b1b612b98ba5df36850fd5f1e54efb8f27b9
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-12-03 07:37:36 +0700

    Update cau4_12.py

--- bai4/cau4_12.py
+++ bai4/cau4_12.py
@@ -1,3 +1,4 @@
+print('họ tên: trần huy vinh; MSSV:245752021610007')
 ds = input("Nhập các phần tử của list (cách nhau bằng khoảng trắng): ").split()
 print("\nList ban đầu:", ds)
 xoa = input("nhập phần tử muốn xoá: ")
@@ -6,3 +7,4 @@ if xoa in ds:
     print("list sau khi xoá phần tử",ds)
 else:
     print("phần tử không tồn tại trong list.")
+

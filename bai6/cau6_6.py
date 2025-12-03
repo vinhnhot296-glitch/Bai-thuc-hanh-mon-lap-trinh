ccd223e16649c4f4026b387a89b7dc3659a23934
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-12-03 07:50:34 +0700

    Update cau6_6.py

--- bai6/cau6_6.py
+++ bai6/cau6_6.py
@@ -1,3 +1,4 @@
+print('họ tên: trần huy vinh; MSSV:245752021610007')
 class IOString:
     def __init__(self):
         self.str1 = ""
@@ -7,3 +8,4 @@ class IOString:
 
     def print_String(self):
         print(self.str1.upper())
+

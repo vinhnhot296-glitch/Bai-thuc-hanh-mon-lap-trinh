8b8bf3307dc9c37f3b2939b33c5eb35d7859cd68
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-12-03 07:39:00 +0700

    Update cau4_17.py

--- bai4/cau4_17.py
+++ bai4/cau4_17.py
@@ -1,3 +1,5 @@
+print('họ tên: trần huy vinh; MSSV:245752021610007')
+
 def tong_uoc_so(x):
     tong = 1  # 1 luôn là ước của mọi số > 1
     for i in range(2, int(x**0.5) + 1):
@@ -11,4 +13,5 @@ n = int(input("Nhập số nguyên n: "))
 for num in range(1, n):
     if tong_uoc_so(num) > num:
         print(num, end=" ")
+
 

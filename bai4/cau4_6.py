66b1118bd60e73d59e3bc54110d69642b7502e83
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-12-03 07:41:33 +0700

    Refactor input handling and add initial print statement

--- bai4/cau4_6.py
+++ bai4/cau4_6.py
@@ -1,4 +1,4 @@
-
+print('họ tên: trần huy vinh; MSSV:245752021610007')
 full_name = input("Nhập họ và tên : ").strip()
 parts = full_name.split()
 if len(parts) == 0:
@@ -10,3 +10,4 @@ else:
     ten = parts[-1]
     print("Họ:", ho)
     print("Tên riêng:", ten)
+

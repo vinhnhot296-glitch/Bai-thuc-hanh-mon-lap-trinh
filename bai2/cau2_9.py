b9e95148501e84206b84e2a824057785cafce326
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-12-03 07:31:13 +0700

    Add student info and refactor string input handling

--- bai2/cau2_9.py
+++ bai2/cau2_9.py
@@ -1,3 +1,4 @@
+print('họ tên: trần huy vinh; MSSV:245752021610007')
 str=input("enter a string:")
 dict ={}
 for n in str:
@@ -7,3 +8,4 @@ for n in str:
     else:
         dict[n] =1
 print (dict)
+

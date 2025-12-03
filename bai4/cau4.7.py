3a6ff688158aa3bec77062d8ea1f203ec4f73594
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-12-03 07:36:47 +0700

    Update cau4.7.py

--- bai4/cau4.7.py
+++ bai4/cau4.7.py
@@ -1,6 +1,8 @@
+print('họ tên: trần huy vinh; MSSV:245752021610007')
 s = input("Nhập chuỗi: ")
 chuoi_moi = ""
 for ch in s:
     if not ch.isdigit():
         chuoi_moi += ch
 print("Chuỗi sau khi loại bỏ chữ số:", chuoi_moi)
+

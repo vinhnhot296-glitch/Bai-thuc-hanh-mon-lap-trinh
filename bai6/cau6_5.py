598a310b56a9edf3493176e8954d9a19ac0d6388
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-12-03 07:50:24 +0700

    Update cau6_5.py

--- bai6/cau6_5.py
+++ bai6/cau6_5.py
@@ -1,6 +1,8 @@
+print('họ tên: trần huy vinh; MSSV:245752021610007')
 class py_solution:
     def reverse_words(self, s):
         return ' '.join(reversed(s.split()))
 
 # Example usage
 print(py_solution().reverse_words('hello .py'))
+

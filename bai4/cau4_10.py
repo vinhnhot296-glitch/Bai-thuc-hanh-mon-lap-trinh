eae547606681731943f7dbe7517fe6c6d1f04bf9
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-12-03 07:37:03 +0700

    Refactor list input and print statements

--- bai4/cau4_10.py
+++ bai4/cau4_10.py
@@ -1,3 +1,4 @@
+print('họ tên: trần huy vinh; MSSV:245752021610007')
 lst = input("Nhập các phần tử của list (cách nhau bằng khoảng trắng): ").split()
 print("\nList ban đầu:", lst)
 if len(lst) > 2:
@@ -5,3 +6,4 @@ if len(lst) > 2:
 else:
     cat_list = []
 print("List sau khi cắt (bỏ phần tử đầu và cuối):", cat_list)
+

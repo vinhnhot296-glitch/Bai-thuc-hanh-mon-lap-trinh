2712da08997e4ddbd6a9baec812b561cc34640e8
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-12-03 07:34:05 +0700

    Refactor checkValue function and add student info

--- bai3/cau3_7.py
+++ bai3/cau3_7.py
@@ -1,6 +1,8 @@
+print('họ tên: trần huy vinh; MSSV:245752021610007')
 def checkValue(n):  
   if n%2 == 0:  
    print ("Đây là một số chẵn")  
   else:  
    print ("Đây là một số lẻ")
 checkValue(6) 
+

68f047313ab1b6e4d8368201de00319214115ed2
vinhnhot296-glitch <vinhnhot.296@gmail.com>
2025-12-03 07:29:09 +0700

    Update cau2_11.py

--- bai2/cau2_11.py
+++ bai2/cau2_11.py
@@ -1,3 +1,4 @@
+print('họ tên: trần huy vinh; MSSV:245752021610007')
 l=[1,'pỵhon',4,7]
 k=['cse',2,'guntur',8]
 m=[]
@@ -6,3 +7,4 @@ m.append(k);
 print(m)
 d={1:1,2:k,'combine_list':m}
 print(d)
+
